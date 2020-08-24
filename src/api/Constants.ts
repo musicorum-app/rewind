8d5efe0bbc49927a3031372b98c5c61e50b3883a
Matheus Dias <methidezeeni@gmail.com>
2020-08-24 10:14:28 -0300

    fix api url

--- src/api/Constants.ts
+++ src/api/Constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_AVATAR = 'https://lastfm.freetls.fastly.net/i/u/avatar670/818148bf682d429dc215c1705eb27b98'
-export const API_URL = 'https://resource.musicorumapp.com//'
+export const API_URL = 'https://resource.musicorumapp.com/'
 export const MAX_TRIES = 3
 export const SHOW_PEDRO_PRINT = 'UDgdDQz'
